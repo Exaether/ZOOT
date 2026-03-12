da4044f09d3254057e855830dbeeea95be5e85b3
Rutile <johan_lefol@yahoo.com>
2026-03-12 14:39:08 +0100

    fix talent description bug
    apparently it is possible to have an empty talent, so added a test for
    that

--- src/lib/utils/blackBoardUtils.ts
+++ src/lib/utils/blackBoardUtils.ts
@@ -1,18 +1,21 @@
-export function parseDesc(str: string, blackboard: { [x: string]: any; }) {
-	return str.replaceAll(/<[@$]([\w.]+)>/g, (match, tag) => {
-		return `<span class="${tag.replaceAll(".", " ")}">`;
-	}).replaceAll(/\{([^}<]+)\}/g, (match, key) => {
-			const [bbKey, format] = key.split(':');
+export function parseDesc(str: string, blackboard: { [x: string]: any }) {
+	return str
+		.replaceAll(/<[@$]([\w.]+)>/g, (match, tag) => {
+			return `<span class="${tag.replaceAll(".", " ")}">`;
+		})
+		.replaceAll(/\{([^}<]+)\}/g, (match, key) => {
+			const [bbKey, format] = key.split(":");
 			let value = blackboard[bbKey];
 
 			if (value === undefined) value = bbKey;
 
 			// Handle formatting (like ":0%")
-			if (format?.endsWith('%')) {
+			if (format?.endsWith("%")) {
 				const decimals = parseInt(format) || 0;
-				value = (value * 100).toFixed(decimals) + '%';
+				value = (value * 100).toFixed(decimals) + "%";
 			}
-			return value
-		}).replaceAll("</>", "</span>")
+			return value;
+		})
+		.replaceAll("</>", "</span>")
 		.replaceAll("\n", "<br>");
 }
